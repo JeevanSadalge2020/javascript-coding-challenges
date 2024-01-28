9687207d7bcbae334afe99401c2ca61b77bffad2
jeevan <jeevanhrx@gmail.com>
2024-01-29 00:32:38 +0530

    Revert "codewars new problems colved"
    This reverts commit 84f71aff3ead37cefcfe940cac7a3332fb8b7c27.

--- codewars/2024/unique-in-order.js
+++
@@ -1,7 +0,0 @@
-// https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript
-// console.log(1);
-var uniqueInOrder = function (iterable) {
-  return iterable.split("");
-};
-
-console.log(uniqueInOrder("AAAABBBCcCDAABCCBB"));
