074139da3cdc165e20a0ff09d97827814c936fcf
jeevan <jeevanhrx@gmail.com>
2024-03-03 19:22:18 +0530

    solved-leetcode-problem-217

---
+++ leetcode/217-contains-duplicate.js
@@ -0,0 +1,12 @@
+var containsDuplicate = function (nums) {
+  let obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (obj[nums[i]]) return true;
+    else obj[nums[i]] = 1;
+  }
+  return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 2, 3, 3, 4]));
+console.log(containsDuplicate([1, 2, 3, 3, 4, 1]));
