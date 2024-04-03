93da18a98a830532540071814499e64eef64f66f
jeevan <jeevanhrx@gmail.com>
2024-04-03 14:19:07 +0530

    leetcode 412

---
+++ leetcode/412.fizz-buzz.js
@@ -0,0 +1,20 @@
+var fizzBuzz = function (n) {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) arr.push("FizzBuzz");
+    else if (i % 3 === 0) arr.push("Fizz");
+    else if (i % 5 === 0) arr.push("Buzz");
+    else arr.push("" + i);
+  }
+  return arr;
+};
+
+console.log(fizzBuzz(0));
+console.log(fizzBuzz(1));
+console.log(fizzBuzz(2));
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(4));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(6));
+console.log(fizzBuzz(7));
+console.log(fizzBuzz(15));
