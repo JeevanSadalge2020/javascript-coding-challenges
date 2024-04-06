3f431931a04c74100861d8305e89fc1988585c25
jeevan <jeevanhrx@gmail.com>
2024-04-06 22:07:02 +0530

    leetcode 1816

---
+++ leetcode/1816.truncate-sentence.js
@@ -0,0 +1,7 @@
+var truncateSentence = function (s, k) {
+  return s.split(" ").slice(0, k).join(" ");
+};
+
+console.log(truncateSentence("Hello how are you Contestant", 4));
+console.log(truncateSentence("What is the solution to this problem", 4));
+console.log(truncateSentence("chopper is not a tanuki", 5));
